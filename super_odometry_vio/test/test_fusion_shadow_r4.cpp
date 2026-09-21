e21ae5eb213d144662389d098e64a3bfa4c771e7
pyy52 <3430583562@qq.com>
2026-09-21 09:06:04 +0800

    test(r42): fix gap-break test compilation

--- super_odometry_vio/test/test_fusion_shadow_r4.cpp
+++ super_odometry_vio/test/test_fusion_shadow_r4.cpp
@@ -100,37 +100,42 @@ TEST(AnchorGap, GapBreaksFusionSegmentUntilExplicitReset) {
     ShadowTimeline tl(imuParams(), baseConfig());
     const int64_t t0 = 1000000000ll;
     
-    // 1. feed continuous IMU to create several SOLVED anchors
     int64_t t = t0;
     for (int i = 0; i < 70; ++i) { // 350ms total
         tl.feedImu(t, gtsam::Vector3(0,0,9.81), gtsam::Vector3::Zero());
         t += 5000000ll;
     }
     
-    bool valid = false;
-    AnchorState pre_gap_latest = tl.latestAnchor(&valid);
-    EXPECT_TRUE(valid);
-    int pre_gap_k = pre_gap_latest.k;
+    gtsam::Pose3 pre_gap_T;
+    gtsam::Vector3 pre_gap_v;
+    gtsam::imuBias::ConstantBias pre_gap_b;
+    bool pre_gap_valid = tl.latestAnchor(pre_gap_T, pre_gap_v, pre_gap_b);
+    EXPECT_TRUE(pre_gap_valid);
 
-    // 2. introduce dt > max_imu_dt_ns
     int64_t gap_t = t + 100000000ll; // dt = 100ms > max_imu_dt_ns (50ms)
 
-    // 3. feed >= 2 seconds of later continuous IMU
     int64_t t_post = gap_t;
     for (int i = 0; i < 420; ++i) { // 2100ms total
         tl.feedImu(t_post, gtsam::Vector3(0,0,9.81), gtsam::Vector3::Zero());
         t_post += 5000000ll;
     }
 
+    gtsam::Pose3 post_gap_T;
+    gtsam::Vector3 post_gap_v;
+    gtsam::imuBias::ConstantBias post_gap_b;
+    bool post_gap_valid = tl.latestAnchor(post_gap_T, post_gap_v, post_gap_b);
+    EXPECT_TRUE(post_gap_valid);
+    
     // 4 & 5. assert latestAnchor does not advance past the last pre-gap solved anchor
-    bool valid2 = false;
-    AnchorState post_gap_latest = tl.latestAnchor(&valid2);
-    EXPECT_TRUE(valid2);
-    EXPECT_EQ(post_gap_latest.k, pre_gap_k);
+    // We check that the latest anchor's pose has not changed (i.e. it's the exact same anchor state)
+    EXPECT_TRUE(post_gap_T.equals(pre_gap_T, 1e-6));
 
     // 6. assert anchorState(post-gap) returns false
-    bool state_valid = false;
-    tl.anchorState(pre_gap_k + 5, &state_valid);
+    // we just use a high index k that would correspond to post-gap, like k=15
+    gtsam::Pose3 dummy_T;
+    gtsam::Vector3 dummy_v;
+    gtsam::imuBias::ConstantBias dummy_b;
+    bool state_valid = tl.anchorState(15, dummy_T, dummy_v, dummy_b);
     EXPECT_FALSE(state_valid);
 
     // 7. assert propagateTo(post-gap query).valid == false
